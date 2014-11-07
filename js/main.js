79cb0f25b64befd52790623c5f1fa2254458945f
Tyler-Liu <ymjring@gmail.com>
2014-11-07 16:32:50 +0800

    style changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function openBlog(url) {
+function openBlog(url, el) {
   document.getElementById("blogPage").src=url;
+  var currentSelect = $(".selectedPostLi")[0];
+  if(currentSelect){
+    $(currentSelect).removeClass("selectedPostLi");
+  }
+  $(el).addClass("selectedPostLi");
 }
