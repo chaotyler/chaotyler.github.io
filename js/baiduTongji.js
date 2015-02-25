98900ef73030cff47a0446311c8e92584bee8b4b
Tyler-Liu <ymjring@gmail.com>
2015-02-25 15:40:22 +0800

    add Baidu Tongji remove useless files

---
+++ js/baiduTongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?4a2be88010ed0fcf8ecbb8ba1c555eda";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
