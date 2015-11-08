9ca70b3e864c9e3730ad8e6010185a48aae12692
Ting Hong <tinghong.lc@alibaba-inc.com>
2015-11-09 00:07:03 +0800

    zoe博客修改

--- zoe/index.js
+++ zoe/index.js
@@ -28,10 +28,7 @@
           }
           self.position--;
         }
-        document.querySelector('.content').style.top = - self.position * 100 + '%';
-        setTimeout(function() {
-          self.canSlide = true;
-        }, self.slideDuration + 500);
+        self.slide();
       });
       document.querySelector('.content').addEventListener('transitionend', function(e) {
         if (e.target !== document.querySelector('.content')) {
@@ -42,70 +39,106 @@
         } else {
           document.querySelector('.down-arrow').style.display = 'block';
         }
-        self.beforeSlideAnimationDonw();
-        self['slideAnimation' + self.position]();
+        // self.beforeSlideAnimationDonw();
+        // self['slideAnimation' + self.position]();
+        self.slideComplete();
       });
-    }, 
+      document.querySelector('.links-con').addEventListener('click', function(e) {
+        var index = Array.prototype.indexOf.call(document.querySelectorAll('.links-con .links'), e.target);
+        if (index === -1) {
+          return;
+        }
+        self.position = index;
+        self.slide();
+      });
+      document.querySelector('.right-nav').addEventListener('click', function(e) {
+        var index = Array.prototype.indexOf.call(document.querySelectorAll('.right-nav .nav'), e.target);
+        if (index === -1) {
+          return;
+        }
+        self.position = index;
+        self.slide();
+      });
+    },
+    slide: function() {
+      var self = this;
+      document.querySelector('.content').style.top = - self.position * 100 + '%';
+      setTimeout(function() {
+        self.canSlide = true;
+      }, self.slideDuration + 500);
+    },
+    slideComplete: function() {
+      document.querySelector('.links-con .links.selected').classList.remove('selected');
+      document.querySelectorAll('.links-con .links')[this.position].classList.add('selected');
+      document.querySelector('.right-nav .nav.selected').classList.remove('selected');
+      document.querySelectorAll('.right-nav .nav')[this.position].classList.add('selected');
+    },
     beforeSlideAnimationDonw: function() {
-      if (this.position !== 1) {
-        document.querySelector('.about-inner').style.visibility = 'hidden';
-      }
-      if (this.position !== 2) {
-        document.querySelector('.works-inner').style.visibility = 'hidden';
-      }
+      // 第一版动画
+      // if (this.position !== 1) {
+      //   document.querySelector('.about-inner').style.visibility = 'hidden';
+      // }
+      // if (this.position !== 2) {
+      //   document.querySelector('.works-inner').style.visibility = 'hidden';
+      // }
     },
     slideAnimation0: function() {
-      document.querySelector('.banner').classList.add('rubberBand');
-      setTimeout(function() {
-        document.querySelector('.banner').classList.remove('rubberBand');
-      }, this.slideDuration + 100);
+      // 第一版动画
+      // document.querySelector('.banner').classList.add('rubberBand');
+      // setTimeout(function() {
+      //   document.querySelector('.banner').classList.remove('rubberBand');
+      // }, this.slideDuration + 100);
     },
     slideAnimation1: function() {
-      var aboutInner = document.querySelector('.about-inner');
-      aboutInner.classList.add('zoomInDown');
-      aboutInner.style.visibility = 'visible';
-      setTimeout(function() {
-        aboutInner.classList.remove('zoomInDown');
-      }, this.slideDuration + 100);
+      // 第一版动画
+      // var aboutInner = document.querySelector('.about-inner');
+      // aboutInner.classList.add('zoomInDown');
+      // aboutInner.style.visibility = 'visible';
+      // setTimeout(function() {
+      //   aboutInner.classList.remove('zoomInDown');
+      // }, this.slideDuration + 100);
     },
     slideAnimation2: function() {
-      var worksInner = document.querySelector('.works-inner');
-      worksInner.classList.add('lightSpeedIn');
-      worksInner.style.visibility = 'visible';
-      setTimeout(function() {
-        worksInner.classList.remove('lightSpeedIn');
-      }, this.slideDuration + 100);
+      // 第一版动画
+      // var worksInner = document.querySelector('.works-inner');
+      // worksInner.classList.add('lightSpeedIn');
+      // worksInner.style.visibility = 'visible';
+      // setTimeout(function() {
+      //   worksInner.classList.remove('lightSpeedIn');
+      // }, this.slideDuration + 100);
     },
     slideAnimation3: function() {
-      var self = this;
-      var list0 = document.querySelector('.collections-list0');
-      list0.classList.add('shake');
-      setTimeout(function() {
-        list0.classList.remove('shake');
-      }, self.slideDuration + 100);
+      // 第一版动画
+      // var self = this;
+      // var list0 = document.querySelector('.collections-list0');
+      // list0.classList.add('shake');
+      // setTimeout(function() {
+      //   list0.classList.remove('shake');
+      // }, self.slideDuration + 100);
 
-      var list1 = document.querySelector('.collections-list1');
-      list1.classList.add('flash');
-      setTimeout(function() {
-        list1.classList.remove('flash');
-      }, self.slideDuration + 100);
+      // var list1 = document.querySelector('.collections-list1');
+      // list1.classList.add('flash');
+      // setTimeout(function() {
+      //   list1.classList.remove('flash');
+      // }, self.slideDuration + 100);
 
-      var listItem2s = document.querySelectorAll('.collections-item2');
-      for (var i = 0; i < listItem2s.length; i++) {
-        (function(index) {
-          listItem2s[index].classList.add('swing');
-          setTimeout(function() {
-            listItem2s[index].classList.remove('swing');
-          }, self.slideDuration + 100);
-        }(i));
-      }
+      // var listItem2s = document.querySelectorAll('.collections-item2');
+      // for (var i = 0; i < listItem2s.length; i++) {
+      //   (function(index) {
+      //     listItem2s[index].classList.add('swing');
+      //     setTimeout(function() {
+      //       listItem2s[index].classList.remove('swing');
+      //     }, self.slideDuration + 100);
+      //   }(i));
+      // }
     },
     slideAnimation4: function() {
-      var contactInner = document.querySelector('.contact-inner');
-      contactInner.classList.add('bounce');
-      setTimeout(function() {
-        contactInner.classList.remove('bounce');
-      }, this.slideDuration + 100);
+      // 第一版动画
+      // var contactInner = document.querySelector('.contact-inner');
+      // contactInner.classList.add('bounce');
+      // setTimeout(function() {
+      //   contactInner.classList.remove('bounce');
+      // }, this.slideDuration + 100);
     }
   };
 
