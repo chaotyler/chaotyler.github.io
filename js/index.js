226eff9526bd27a4a021c59674678e20735a4556
Ting Hong <tinghong.lc@alibaba-inc.com>
2015-12-25 14:59:05 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 (function(window) {
+  // 用于监测是否能访问twitter
   var twitterImg = new Image();
   twitterImg.src = 'https://twitter.com/favicon.ico';
   
+  // 超过5秒没成功即不能访问twitter，则使用抠下来的twitter follow button，不加载timeline
   var twitterTimeout = setTimeout(function() {
     twitterImg.src = '';
     document.getElementById('twitter-follow-gfw').style.display = 'block';
   }, 5000);
 
+  // 能访问twitter，则加载follow按钮和twitter timeline
   twitterImg.onload = function() {
     clearTimeout(twitterTimeout);
     loadTwitterWidget(document, 'script', 'twitter-wjs');
@@ -19,6 +22,7 @@
   //   document.getElementById('twitter-follow-gfw').style.display = 'block';
   // }
 
+  // 用于加载twitter组件的方法，抠自twitter。
   function loadTwitterWidget(d,s,id){
     var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
     if(!d.getElementById(id)){
