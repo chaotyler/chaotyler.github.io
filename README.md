3ad7c43fe6f9aa1aa8501017fff31ae83b37eb7d
Ting Hong <tinghong.lc@alibaba-inc.com>
2015-12-25 15:07:33 +0800

    删除原来的文件，启用新blog

---
+++ README.md
@@ -0,0 +1,1 @@
+Chaotyler Blog
--- js/main.js
+++
@@ -1,8 +0,0 @@
-function openBlog(url, el) {
-  document.getElementById("blogPage").src=url;
-  var currentSelect = $(".selectedPostLi")[0];
-  if(currentSelect){
-    $(currentSelect).removeClass("selectedPostLi");
-  }
-  $(el).addClass("selectedPostLi");
-}
