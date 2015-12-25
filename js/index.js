2c1e5684ee44fb0e8aa3c9558d960906314eb27c
Ting Hong <tinghong.lc@alibaba-inc.com>
2015-12-25 17:21:28 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,34 +1,48 @@
 (function(window) {
-  // 用于监测是否能访问twitter
-  var twitterImg = new Image();
-  twitterImg.src = 'https://twitter.com/favicon.ico';
+
+  var ua = navigator.userAgent;
+  var isMobile = ua.search(/Android|iPhone/i) != -1;
   
-  // 超过5秒没成功即不能访问twitter，则使用抠下来的twitter follow button，不加载timeline
-  var twitterTimeout = setTimeout(function() {
-    twitterImg.onerror = null;
-    twitterImg.onload = null;
-    twitterImg.src = '';
+  // 如果是移动端，不需要监测是否能访问twitter，直接用抠下来的follow按钮
+  if (isMobile) {
     document.getElementById('twitter-follow-gfw').style.display = 'block';
-  }, 5000);
+  } else {
+    checkTwitter();
+  }
 
-  // 能访问twitter，则加载follow按钮和twitter timeline
-  twitterImg.onload = function() {
-    clearTimeout(twitterTimeout);
-    loadTwitterWidget(document, 'script', 'twitter-wjs');
-    document.getElementById('twitter-follow').style.display = 'block';
-    document.getElementById('twitter-timeline').style.display = 'block';
-  };
+  // 检查是否能访问twitter，并按照结果来加载页面
+  function checkTwitter() {
+    // 用于监测是否能访问twitter
+    var twitterImg = new Image();
+    twitterImg.src = 'https://twitter.com/favicon.ico';
+    
+    // 超过5秒没成功即不能访问twitter，则使用抠下来的twitter follow button，不加载timeline
+    var twitterTimeout = setTimeout(function() {
+      twitterImg.onerror = null;
+      twitterImg.onload = null;
+      twitterImg.src = '';
+      document.getElementById('twitter-follow-gfw').style.display = 'block';
+    }, 5000);
 
-  // error时的处理
-  twitterImg.onerror = function() {
-    clearTimeout(twitterTimeout);
-    document.getElementById('twitter-follow-gfw').style.display = 'block';
-  };
+    // 能访问twitter，则加载follow按钮和twitter timeline
+    twitterImg.onload = function() {
+      clearTimeout(twitterTimeout);
+      loadTwitterWidget(document, 'script', 'twitter-wjs');
+      document.getElementById('twitter-follow').style.display = 'block';
+      document.getElementById('twitter-timeline').style.display = 'block';
+    };
 
-  // safari在twitterImg设置src为空时仍然会继续加载很长一段时间，故设置最长加载20s，超时直接停掉
-  setTimeout(function() {
-    window.stop && window.stop();
-  }, 200000);
+    // error时的处理
+    twitterImg.onerror = function() {
+      clearTimeout(twitterTimeout);
+      document.getElementById('twitter-follow-gfw').style.display = 'block';
+    };
+
+    // safari在twitterImg设置src为空时仍然会继续加载很长一段时间，故设置最长加载20s，超时直接停掉
+    // setTimeout(function() {
+    //   window.stop && window.stop();
+    // }, 200000);
+  }
 
   // 用于加载twitter组件的方法，抠自twitter。
   function loadTwitterWidget(d,s,id){
